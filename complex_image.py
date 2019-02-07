393c91e6ffc01dad79d2e0cdb6630160e264818e
ashan <chanaka.16@cse.mrt.ac.lk>
2019-02-07 09:41:51 -0500

    seond

--- complex_image.py
+++ complex_image.py
@@ -1,2 +1,22 @@
-print 123
-sdsdsd
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('d.jpeg',0)
+
+img = cv2.medianBlur(img,5)
+
+ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,11,2)
+th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+
+titles = ['Original Image', 'Global Thresholding (v = 127)',
+            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+images = [img, th1, th2, th3]
+m = cv2.medianBlur(th3,5)
+cv2.imshow('image',m)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
